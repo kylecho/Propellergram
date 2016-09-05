0df2a49cb31220e5edd38e31cf82108306087368
Kyle Cho <kylecho.work@gmail.com>
2016-09-05 09:58:20 -0700

    add validation to posts; add time-ago to post view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :image, presence: true
+  validates :caption, length: { minimum: 3, maximum: 300 }
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
