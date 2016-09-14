82145325c507f5d861ffbeefe9eec30a6f650936
Kyle Cho <kylecho.work@gmail.com>
2016-09-13 19:10:20 -0700

    create a user profile to show created posts

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,5 @@
+class ProfilesController < ApplicationController
+  def show
+    @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts do
     resources :comments
   end
+
+  get ':user_name', to: 'profiles#show', as: :profile
 
   root 'posts#index'
 end
