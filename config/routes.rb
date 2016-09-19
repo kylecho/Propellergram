15d8c98bbb5f8066200fa9dfd1ba112bc96aed7c
Kyle Cho <kylecho.work@gmail.com>
2016-09-18 20:42:37 -0700

    add notification index view

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'notifications', to: 'notifications#index'
+  get 'notifications/:id/link_through', to: 'notifications#link_through', as: :link_through
   get ':user_name', to: 'profiles#show', as: :profile
   get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
   patch ':user_name/edit', to: 'profiles#update', as: :update_profile
